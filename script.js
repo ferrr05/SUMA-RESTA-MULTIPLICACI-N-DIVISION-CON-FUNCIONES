b701f8e6bd4182b568795a092cf50380cee6df11
ferrr05 <123724312+ferrr05@users.noreply.github.com>
2023-04-19 13:10:05 +0200

    calculadora

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-
-function sumar(num1, num2) {
-    let resultado = num1 + num2;
-    console.log("El resultado de la suma es: " + resultado);
-}
-
-
-function restar(num1, num2) {
-    let resultado = num1 - num2;
-    console.log("El resultado de la resta es: " + resultado);
-}
-
-
-function multiplicar(num1, num2) {
-    let resultado = num1 * num2;
-    console.log("El resultado de la multiplicación es: " + resultado);
-}
-
-
-function dividir(num1, num2) {
-    let resultado = num1 / num2;
-    console.log("El resultado de la división es: " + resultado);
-}
-
-
-let numero1 = parseInt(prompt("Introduce el primer número: "));
-let numero2 = parseInt(prompt("Introduce el segundo número: "));
-
-sumar(numero1, numero2);
-restar(numero1, numero2);
-multiplicar(numero1, numero2);
-dividir(numero1, numero2);
+function suma(num1, num2) {
+    return num1 + num2;
+  }
+  
+  function resta(num1, num2) {
+    return num1 - num2;
+  }
+  
+  function multiplicacion(num1, num2) {
+    return num1 * num2;
+  }
+  
+  function division(num1, num2) {
+    if (num2 === 0) {
+      return "No se puede dividir por cero";
+    } else {
+      return num1 / num2;
+    }
+  }
+  
+  function startCalculator() {
+    const num1 = parseInt(prompt("Introduce el primer número"));
+    const num2 = parseInt(prompt("Introduce el segundo número"));
+  
+    console.log(`La suma de ${num1} y ${num2} es: ${suma(num1, num2)}`);
+    console.log(`La resta de ${num1} y ${num2} es: ${resta(num1, num2)}`);
+    console.log(`La multiplicación de ${num1} y ${num2} es: ${multiplicacion(num1, num2)}`);
+    console.log(`La división de ${num1} y ${num2} es: ${division(num1, num2)}`);
+  }
+  
+  startCalculator();
+  
