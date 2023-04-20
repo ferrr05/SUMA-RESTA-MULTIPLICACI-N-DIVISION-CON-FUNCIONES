e8b59b7d0c4ffbe8880420604bcb399b0793cdd8
ferrr05 <123724312+ferrr05@users.noreply.github.com>
2023-04-20 21:23:15 +0200

    cambios

--- script.js
+++ script.js
@@ -1,32 +1,37 @@
-function suma(num1, num2) {
-    return num1 + num2;
-  }
-  
-  function resta(num1, num2) {
-    return num1 - num2;
-  }
-  
-  function multiplicacion(num1, num2) {
-    return num1 * num2;
-  }
-  
-  function division(num1, num2) {
-    if (num2 === 0) {
-      return "No se puede dividir por cero";
-    } else {
-      return num1 / num2;
-    }
-  }
-  
-  function startCalculator() {
-    const num1 = parseInt(prompt("Introduce el primer número"));
-    const num2 = parseInt(prompt("Introduce el segundo número"));
-  
-    console.log(`La suma de ${num1} y ${num2} es: ${suma(num1, num2)}`);
-    console.log(`La resta de ${num1} y ${num2} es: ${resta(num1, num2)}`);
-    console.log(`La multiplicación de ${num1} y ${num2} es: ${multiplicacion(num1, num2)}`);
-    console.log(`La división de ${num1} y ${num2} es: ${division(num1, num2)}`);
-  }
-  
-  startCalculator();
-  
+calcular ( ) ;
+función  calcular ( )  {
+    let  num1  =  parseInt ( prompt ( "Primer número:" ) ) ;
+    let  num2  =  parseInt ( prompt ( "Segundo número:" ) ) ;
+
+    sumar ( num1 ,  num2 ) ;
+    reiniciar ( num1 ,  num2 ) ;
+    multiplicar ( num1 ,  num2 ) ;
+    dividir ( num1 ,  num2 ) ;
+}
+
+función  sumar ( num1 ,  num2 )  {
+    let  resultado  =  num1  +  num2 ;
+    consola _ log ( `La suma es: ${ resultado } ` ) ;
+    alert ( "El resultado de la suma es: "  +  suma ) ;
+}
+
+
+función  reiniciar ( num1 ,  num2 )  {
+    let  resultado  =  num1  -  num2 ;
+    consola _ log ( `La resta es: ${ resultado } ` ) ;
+    alert ( "El resultado de la resta es: "  +  resta ) ;
+}
+
+función  multiplicar ( num1 ,  num2 )  {
+    let  resultado  =  num1  *  num2 ;
+    consola _ log ( `La multiplicación es: ${ resultado } ` ) ;
+    alert ( "El resultado de la multiplicación es: "  +  multiplicación ) ;
+}
+
+
+función  dividir ( num1 ,  num2 )  {
+    let  resultado  =  num1  /  num2 ;
+    consola _ log ( `La división es: ${ resultado } ` ) ;
+    alert ( "El resultado de la división es: "  +  división ) ;
+
+}
